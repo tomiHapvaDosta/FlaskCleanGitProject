5e0c7e75442f49ba0c06269daa9387d570af6dd7
Tomislav Simeonov <filipsim1@abv.bg>
2026-02-26 22:27:44 +0200

    676767

--- app/db.py
+++ app/db.py
@@ -8,30 +8,15 @@ class Base(DeclarativeBase):
 class User(SQLAlchemyBaseUserTableUUID, Base):
     pass
 
-class BudgetProfile(Base):
-    __tablename__ = 'budget_profile'
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(UUID(as_uuid=True), ForeignKey('user.id'), nullable=False)
-
-    monthly_income = Column(DECIMAL(12, 2), nullable=False)
-    min_savings_rate = Column(DECIMAL(5, 4), nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
-
-    fixed_expenses = relationship('FixedExpense', back_populates='budget_profile')
-
-class FixedExpense(Base):
-    __tablename__ = 'fixed_expense'
+class Habit(Base):
+    __tablename__ = 'habit'
 
     id = Column(UUID(as_uuid=True), primary_key=True)
-    budget_profile_id = Column(UUID(as_uuid=True), ForeignKey('budget_profile.id'), nullable=False)
-
     name = Column(String(100), nullable=False)
-    amount = Column(DECIMAL(12, 2), nullable=False)
-
+    target_per_week = Column(Integer, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
 
-    budget_profile = relationship('BudgetProfile', back_populates='fixed_expense')
+
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
--- app/settings.py
+++ app/settings.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from pydantic import BaseModel
 from fastapi_users import schemas, BaseUserManager, FastAPIUsers, UUIDIDMixin, models
 from collections.abc import AsyncGenerator
-from sqlalchemy import Column, String, Text, DateTime, ForeignKey, select, Float, DECIMAL
+from sqlalchemy import Column, String, Text, DateTime, ForeignKey, select, Float, DECIMAL, Integer, Date
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
